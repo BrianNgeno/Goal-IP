9548b08390298c189f2cedfbbb8dc4ad5675a118
brianngeno <bkn.ngeno@gmail.com>
2018-09-07 11:03:18 +0300

    added files and folders to be ignored on push

--- Goal-IP/config.py
+++ Goal-IP/config.py
@@ -1,17 +1,28 @@
 import os 
 class Config:
+    '''
+    parent class config 
+    '''
    
     @staticmethod
     def init__app(app):
         pass
  
 class ProdConfig(Config):
+    '''
+    child class production and takes in config from the parent
+    '''
     pass
 
 class DevConfig(Config):
     DEBUG = True
 
+# defining our config options
+class TestConfig(Config):
+    pass
+
 config_options ={
     'production':ProdConfig,
-    'development' :DevConfig
+    'development' :DevConfig,
+    'test': TestConfig
 }
