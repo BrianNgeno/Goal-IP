adf5cb37f191e68f8c594d4af6926535184aca11
brianngeno <bkn.ngeno@gmail.com>
2018-09-08 17:12:16 +0300

    added @manage shell decorator to create a shell function and the make_shell_context function

--- Goal-IP/manage.py
+++ Goal-IP/manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 
 #creating app instance
 app = create_app('development')
@@ -19,6 +20,10 @@ def test():
 
 def make_shell_context():
     return dict(app = app)
+
+@manage.shell
+def make_shell_context():
+    return dict (app = app, db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
