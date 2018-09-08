582611945dd0d1ea828bf3747673b82630cd5ed1
brianngeno <bkn.ngeno@gmail.com>
2018-09-08 20:18:42 +0300

    installed flask-login

--- Goal-IP/manage.py
+++ Goal-IP/manage.py
@@ -1,12 +1,15 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
-
-#creating app instance
+from app.models import User, Role
+from  flask_migrate import Migrate, MigrateCommand
+# creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command 
 def test():
@@ -20,7 +23,6 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict (app = app, db = db,User = User, Role = Role)
-
+    return dict(app = app,db = db,User = User, Role = Role )
 if __name__ == '__main__':
     manager.run()
