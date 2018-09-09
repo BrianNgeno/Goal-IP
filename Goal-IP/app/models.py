364bc77f0fd415c5ab154a6bb1ac2241b01dff93
brianngeno <bkn.ngeno@gmail.com>
2018-09-09 23:30:34 +0300

    added table pitches to the database and added six columns to it

--- Goal-IP/app/models.py
+++ Goal-IP/app/models.py
@@ -74,4 +74,19 @@ class PitchCategory(db.Model):
         categories = PitchCategory.query.all()
         return categories
 
-    
+class Pitches(db.Model):
+
+    """
+    Class that holds instances of all the pitches in the different categories
+    """
+    all_pitches = []
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    actual_pitch = db.Column(db.String)
+    date_posted = db.Column(db.DateTime, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("pitch_categories.id"))
+    comment = db.relationship("Comments", backref="pitches", lazy="dynamic")
+   
