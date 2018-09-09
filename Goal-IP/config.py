b73e6b2bcbda2429bd6f4e4d435d72f16a3e051e
brianngeno <bkn.ngeno@gmail.com>
2018-09-09 21:45:46 +0300

    added mail service and added a class email.py

--- Goal-IP/config.py
+++ Goal-IP/config.py
@@ -4,7 +4,14 @@ class Config:
     parent class config 
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bknngeno:123@localhost/pitches'
-   
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("lukemark@gmail.com")
+    MAIL_PASSWORD = os.environ.get("1234qwer,./")
+
     @staticmethod
     def init__app(app):
         pass
