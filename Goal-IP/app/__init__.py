b6c265bfcc95d0ad7a3d8bc3f0b9fb09f50e9647
brianngeno <bkn.ngeno@gmail.com>
2018-09-09 23:43:57 +0300

    injected pitches and category pitch to the manage shell

--- Goal-IP/app/__init__.py
+++ Goal-IP/app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
-
+from flask_moment import Moment
 
 
 bootstrap = Bootstrap()
