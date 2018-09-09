781f1757aaecf96a8ce5b818eab653c2a2e08313
brianngeno <bkn.ngeno@gmail.com>
2018-09-09 23:27:53 +0300

    added a form to add pitch

--- Goal-IP/app/main/forms.py
+++ Goal-IP/app/main/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validator import Required
-
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+from wtforms import ValidationError
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    content = TextAreaField('Your pi')
+    submit = SubmitField('SUBMIT')
